becf1dd3663b0e3f7572128ccea801d5ae229ad1
RO <49290325+juggao@users.noreply.github.com>
2023-06-17 04:40:31 +0200

    Add files via upload
    rewrite with pygame

--- tt-ticker.py
+++ tt-ticker.py
@@ -7,14 +7,62 @@ import time
 import sys
 import os
 import subprocess 
+import pygame as pg
+  
 
 newsstring=""
-deli = 68          # milliseconds of delay per character
+deli = 133          # milliseconds of delay per character
 root = tk.Tk()
 svar = tk.StringVar()
 fnt = TkFont.Font(family="Helvetica",size=25,weight="bold",underline=0)
 labl = tk.Label(root, textvariable=svar, height=1, font=fnt, width=200)
 stop=False
+
+
+class TTscroll:
+    def __init__(self, screen_rect, lst):
+        self.srect = screen_rect
+        self.lst = lst
+        self.size = 18
+        self.color = (0,0,0)
+        self.buff_centery = self.srect.height/2 + 5
+        #self.buff_centery = self.srect.h
+        self.buff_lines = 50
+        self.timer = 0.0
+        self.delay = 0
+        self.make_surfaces()
+    
+    def make_text(self,message):
+        global newsstring
+        font = pg.font.SysFont('Liberation Sans', self.size)
+        text = font.render(newsstring,True,self.color)
+        #rect = text.get_rect(center = (self.srect.w, self.srect.centery + self.buff_centery) )
+        rect = text.get_rect(center = (self.srect.w, self.srect.centery))
+        return text,rect
+  
+    def make_surfaces(self):
+        global newsstring
+        self.text = []
+        for i, line in enumerate(newsstring):
+            l = self.make_text(line)
+            l[1].y =  self.srect.height/2 - 7
+            l[1].x =  self.srect.w
+            self.text.append(l)
+  
+    def update(self):
+        if pg.time.get_ticks()-self.timer > self.delay:
+            self.timer = pg.time.get_ticks()
+            for text, rect in self.text:
+                rect.x -= 2
+            if rect.x <= -(rect.w):
+                print("center")    
+                self.make_surfaces()
+                
+    def render(self, surf):
+        for text, rect in self.text:
+            surf.blit(text, rect)
+  
+
 
 def updatenews():
     global newsstring
@@ -29,18 +77,13 @@ def updatenews():
             else:
                 print("NEW NEWS")
                 #print(newss)
-                shif.msg = newss
+                #shif.msg = newss
             time.sleep(10)
             if stop == True:
                 return
     except KeyboardInterrupt:
         return
     
-def shif():
-    shif.msg = shif.msg[1:] + shif.msg[0]
-    svar.set(shif.msg)
-    root.after(deli, shif)
-
 def tt():
     subprocess.call(['sh', '/home/reinold/bin/ttdownload.sh'])
     subprocess.call(['sh', '/home/reinold/bin/extract.sh'])
@@ -58,30 +101,49 @@ def tt():
 def main():
     #   tk.Button(root, text="Click Me", command=click_btn).pack()
     global newsstring
+   
+    x = 0
+    y = 1080
+    from screeninfo import get_monitors
+    for m in get_monitors():
+        y = m.height-35
+        print(str(m))
     
-    emptyMenu = Menu(root)
-    root.config(menu=emptyMenu)
-    root.wm_attributes('-type', 'splash') 
-    root.wm_attributes('-alpha',  1.0, '-fullscreen',  False, '-topmost',  True)
-    root.overrideredirect(False)
+    import os
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+  
+    pg.init() 
     
-    newsstring = tt()      
-    print("FIRST: " , newsstring)  
-    shif.msg=newsstring 
-    shif()
-    labl.pack()
-    root.geometry("+1+1050")
-    root.configure(bg='blue')
     thread = threading.Thread(target=updatenews)
     thread.start()
     
-    try:
-        root.mainloop()
-    except KeyboardInterrupt:
-        stop=True
-        sys.exit(0)
+    newsstring = tt()      
+    print("FIRST: " , newsstring)  
+
+    print(pg.display.Info().current_w)
+    screen = pg.display.set_mode(size=(pg.display.Info().current_w,35), flags=pg.NOFRAME)
+#   screen = pg.display.set_mode(size=(pg.display.Info().current_w,35))
+  
+   
+    screen_rect = screen.get_rect()
+    clock = pg.time.Clock()
+    done = False
+    cred = TTscroll(screen_rect, newsstring)    
+            
+    while not done:
+        for event in pg.event.get(): 
+            if event.type == pg.QUIT:
+                done = True
+        screen.fill((255,255,255))
+        cred.update()
+        cred.render(screen)
+        pg.display.update()
+        clock.tick(60)
+    
         
 if __name__ == "__main__":
     main()
 
+
+  
 
